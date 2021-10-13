603884eb889f270e66b225ec485e5b866cb9a0ad
liupeidong <liupeidong@B-3548MD6R-2021.local>
2021-10-13 19:04:14 +0800

    ok

--- src/main/scala/spark_streaming/WordCount.scala
+++ src/main/scala/spark_streaming/WordCount.scala
@@ -5,7 +5,7 @@ import org.apache.spark.streaming.{Seconds, StreamingContext}
 
 object WordCount {
   def main(args: Array[String]): Unit = {
-    val conf = new SparkConf().setMaster("local[2]").setAppName("SparkStreamingTest")
+    val conf = new SparkConf().setMaster("local[2]").setAppName("WordCount")
     val ssc = new StreamingContext(conf,Seconds(5))
 
     val lines = ssc.socketTextStream("localhost", 7777)
--- src/main/scala/test/SparkStreamingTest.scala
+++ src/main/scala/test/SparkStreamingTest.scala
@@ -1,15 +1,26 @@
 package test
 
-import org.apache.spark.SparkConf
+import org.apache.spark.{HashPartitioner, SparkConf, SparkContext}
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 
 object SparkStreamingTest {
+
+  val updateFunc = (iter:Iterator[(String,Seq[Int],Option[Int])]) => {
+        iter.flatMap{
+          case (key,seq,opt) => Some(seq.sum + opt.getOrElse(0)).map(count => (key, count))
+        }
+  }
+
   def main(args: Array[String]): Unit = {
-    val conf = new SparkConf().setMaster("local[2]").setAppName("SparkStreamingTest")
+    val conf = new SparkConf().setMaster("local[2]").setAppName("WordCountSum")
+    val sc = new SparkContext(conf)
     val ssc = new StreamingContext(conf,Seconds(5))
+    sc.setCheckpointDir("/Users/liupeidong/Desktop")
+    ssc.sparkContext.setLogLevel("WARN")
 
     val lines = ssc.socketTextStream("localhost", 7777)
-    val result = lines.flatMap(_.split(" ")).map((_, 1)).reduceByKey(_ + _)
+    val tuple = lines.flatMap(_.split(" ")).map((_, 1))
+    val result = tuple.updateStateByKey(updateFunc,new HashPartitioner(sc.defaultParallelism),rememberPartitioner = true)
     result.print()
 
     ssc.start()
