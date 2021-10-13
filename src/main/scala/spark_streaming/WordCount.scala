3dfd3e9f4677f424f50b30ddf6c91b2288dafba1
liupeidong <liupeidong@B-3548MD6R-2021.local>
2021-10-13 17:44:10 +0800

    stream word count

--- src/main/scala/spark_streaming/WordCount.scala
+++ src/main/scala/spark_streaming/WordCount.scala
@@ -1,9 +1,9 @@
-package test
+package spark_streaming
 
 import org.apache.spark.SparkConf
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 
-object SparkStreamingTest {
+object WordCount {
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().setMaster("local[2]").setAppName("SparkStreamingTest")
     val ssc = new StreamingContext(conf,Seconds(5))
