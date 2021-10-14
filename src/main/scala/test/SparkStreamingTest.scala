8832659e80b6234a659afe109de18f5ec6bf5f45
liupeidong <liupeidong@B-3548MD6R-2021.local>
2021-10-14 20:14:59 +0800

    all

--- src/main/scala/test/SparkStreamingTest.scala
+++ src/main/scala/test/SparkStreamingTest.scala
@@ -5,24 +5,28 @@ import org.apache.spark.streaming.{Seconds, StreamingContext}
 
 object SparkStreamingTest {
 
-  val updateFunc = (iter:Iterator[(String,Seq[Int],Option[Int])]) => {
-        iter.flatMap{
-          case (key,seq,opt) => Some(seq.sum + opt.getOrElse(0)).map(count => (key, count))
-        }
-  }
-
   def main(args: Array[String]): Unit = {
+    // 环境配置
     val conf = new SparkConf().setMaster("local[2]").setAppName("WordCountSum")
     val sc = new SparkContext(conf)
-    val ssc = new StreamingContext(conf,Seconds(5))
+    val ssc = new StreamingContext(sc,Seconds(5))
     sc.setCheckpointDir("/Users/liupeidong/Desktop")
     ssc.sparkContext.setLogLevel("WARN")
 
+    // 状态更新函数
+    val updateFunc = (iter:Iterator[(String,Seq[Int],Option[Int])]) => {
+      iter.flatMap{
+        case (key,seq,opt) => Some(seq.sum + opt.getOrElse(0)).map(count => (key, count))
+      }
+    }
+
+    // 业务代码 nc -lk 7777
     val lines = ssc.socketTextStream("localhost", 7777)
     val tuple = lines.flatMap(_.split(" ")).map((_, 1))
     val result = tuple.updateStateByKey(updateFunc,new HashPartitioner(sc.defaultParallelism),rememberPartitioner = true)
     result.print()
 
+    // 执行
     ssc.start()
     ssc.awaitTermination()
   }
